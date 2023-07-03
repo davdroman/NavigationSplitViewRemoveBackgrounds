11e1a338b78601d0534e28fa89b3ea1b02522897
David Roman <2538074+davdroman@users.noreply.github.com>
2023-07-03 15:17:51 +0100

    initial commit

---
+++ NavigationSplitViewRemoveBackgrounds.swiftpm/MyApp.swift
@@ -0,0 +1,60 @@
+import NavigationTransitions
+import SwiftUI
+import SwiftUIIntrospect
+
+@main
+struct App: SwiftUI.App {
+    var body: some Scene {
+        WindowGroup {
+			AppView()
+        }
+    }
+}
+
+struct Item: Hashable, Identifiable {
+	let id = UUID()
+	let name: String
+}
+
+struct AppView: View {
+	@State var menuItems: [Item] = [.init(name: "hello"), .init(name: "world")]
+	@State var selectedItem: Item?
+
+	var body: some View {
+		ZStack {
+			Rectangle().fill(.blue.gradient).ignoresSafeArea() // this is a placeholder for your background night sky image
+			NavigationSplitView {
+				List(menuItems, selection: $selectedItem) { item in
+					NavigationLink(value: item) {
+						Text(item.name)
+					}
+					.listRowBackground(Color.clear)
+				}
+				.scrollContentBackground(.hidden)
+				.navigationTransition(.slide)
+			} detail: {
+				Text("Test View")
+			}
+			.tint(.white)
+			.introspect(.navigationSplitView, on: .iOS(.v17)) { split in
+				let removeBackgrounds = {
+					split.viewControllers.forEach { controller in
+						controller.parent?.view.backgroundColor = .clear
+						controller.view.clearBackgrounds()
+					}
+				}
+				removeBackgrounds() // run now...
+				DispatchQueue.main.async(execute: removeBackgrounds) // ... and on the next run loop pass
+			}
+		}
+	}
+}
+
+extension UIView {
+	fileprivate func clearBackgrounds() {
+		backgroundColor = .clear
+		for subview in subviews {
+			subview.clearBackgrounds()
+		}
+	}
+}
